8b24eeab7c0df450048118b25d8b55b6012b34ee
wim van laer <van.laer.wim@gmail.com>
2019-10-26 17:51:44 +0200

    feat/control/logging: created demo view for logging

--- WSharp.Wpf/WSharp.Wpf.NetFramework.Demo/Views/MainWindow.xaml.cs
+++ WSharp.Wpf/WSharp.Wpf.NetFramework.Demo/Views/MainWindow.xaml.cs
@@ -1,10 +1,19 @@
-﻿namespace WSharp.Wpf.NetFramework.Demo.Views
+﻿using WSharp.Wpf.NetFramework.Demo.ViewModels;
+
+namespace WSharp.Wpf.NetFramework.Demo.Views
 {
     public partial class MainWindow
     {
-        public MainWindow()
+        public MainWindow(MainViewModel viewModel)
         {
             InitializeComponent();
+            ViewModel = viewModel;
+        }
+
+        public MainViewModel ViewModel
+        {
+            get => DataContext as MainViewModel;
+            set => DataContext = value;
         }
     }
 }
