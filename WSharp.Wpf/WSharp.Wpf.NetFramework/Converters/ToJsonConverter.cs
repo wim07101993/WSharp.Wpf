33cfadd72ba213d90ba3f0ffb78ddd71eab3019d
wim van laer <van.laer.wim@gmail.com>
2019-10-26 17:31:23 +0200

    feat/control/logging: added converters for the syle

---
+++ WSharp.Wpf/WSharp.Wpf.NetFramework/Converters/ToJsonConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using WSharp.Extensions;
+
+namespace WSharp.Wpf.NetFramework.Converters
+{
+    public class ToJsonConverter : IValueConverter
+    {
+        public static ToJsonConverter Instance { get; } = new ToJsonConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) 
+            => value?.SerializeJson();
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => value?.ToString().DeserializeJson(targetType);
+    }
+}
