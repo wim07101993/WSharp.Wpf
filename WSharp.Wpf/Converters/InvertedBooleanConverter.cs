54b7e7d91fea655dd298ee9637b9a49ca896a2c1
wim van laer <van.laer.wim@gmail.com>
2019-11-20 20:14:26 +0100

    feature/converters: created InvertedBooleanConverter

---
+++ WSharp.Wpf/Converters/InvertedBooleanConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WSharp.Wpf.Converters
+{
+    public class InvertedBooleanConverter : IValueConverter
+    {
+        private static InvertedBooleanConverter _instance;
+        public static InvertedBooleanConverter Instance => _instance ?? (_instance = new InvertedBooleanConverter());
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => value is bool b && !b;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => value is bool b && !b;
+    }
+}
