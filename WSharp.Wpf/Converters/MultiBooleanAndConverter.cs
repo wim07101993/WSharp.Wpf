4deae77e8d4da36013172e4ceb9ec73e4c86b803
wim van laer <van.laer.wim@gmail.com>
2019-11-20 20:17:19 +0100

    feature/converters: created MultiBooleanAndConverter

---
+++ WSharp.Wpf/Converters/MultiBooleanAndConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WSharp.Wpf.Converters
+{
+    public class MultiBooleanAndConverter : IMultiValueConverter
+    {
+        private static MultiBooleanAndConverter _instance;
+        public static MultiBooleanAndConverter Instance => _instance ?? (_instance = new MultiBooleanAndConverter());
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+            => values?.All(x => x is bool b && b) == true;
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+            => new[] { DependencyProperty.UnsetValue };
+    }
+}
