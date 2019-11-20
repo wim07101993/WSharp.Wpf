ba804c8c100aa6b139b017a3982c83a2b0082c0b
wim van laer <van.laer.wim@gmail.com>
2019-11-20 20:34:25 +0100

    feat/object-browser: added theme

---
+++ WSharp.Wpf/Helpers/BindingProxy.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace WSharp.Wpf.Helpers
+{
+    public class BindingProxy : Freezable
+    {
+        public static readonly DependencyProperty DataProperty =
+            DependencyProperty.Register(
+                nameof(Data),
+                typeof(object),
+                typeof(BindingProxy),
+                new PropertyMetadata(null));
+
+        protected override Freezable CreateInstanceCore() => new BindingProxy();
+
+        public object Data
+        {
+            get { return GetValue(DataProperty); }
+            set { SetValue(DataProperty, value); }
+        }
+    }
+}
