b81c1a843281e3d68c53f4b81c7c93d7e188df3c
wim van laer <van.laer.wim@gmail.com>
2019-12-04 10:35:10 +0100

    feature/controls: moved logs view and entry to material project

--- WSharp.Wpf/Controls/LogEntry.cs
+++ WSharp.Wpf/Controls/LogEntry.cs
@@ -1,25 +1,13 @@
 ﻿using System.Windows;
-using System.Windows.Controls;
 using WSharp.Logging;
 
 namespace WSharp.Wpf.Controls
 {
-    public class LogEntry : Control
+    public class LogEntry : ValueControl<ILogEntry>
     {
-        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
-            nameof(Value),
-            typeof(ILogEntry),
-            typeof(LogEntry));
-
         static LogEntry()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(LogEntry), new FrameworkPropertyMetadata(typeof(LogEntry)));
-        }
-
-        public ILogEntry Value
-        {
-            get => (ILogEntry)GetValue(ValueProperty);
-            set => SetValue(ValueProperty, value);
         }
     }
 }
