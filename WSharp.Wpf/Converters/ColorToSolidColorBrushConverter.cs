d6036f4e00e884060e83e046e0660129dd5c761f
wim van laer <van.laer.wim@gmail.com>
2020-01-04 19:37:44 +0100

    Controls: updated picker demo

---
+++ WSharp.Wpf/Converters/ColorToSolidColorBrushConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using System.Windows.Media;
+using WSharp.Wpf.Converters.Bases;
+
+namespace WSharp.Wpf.Converters
+{
+    public class ColorToSolidColorBrushConverter : ATypedValueConverter<Color, SolidColorBrush>
+    {
+        private static ColorToSolidColorBrushConverter instance;
+        public static ColorToSolidColorBrushConverter Instance => instance ??= new ColorToSolidColorBrushConverter();
+
+        protected override bool TInToTOut(Color tin, object parameter, CultureInfo culture, out SolidColorBrush tout)
+        {
+            tout = new SolidColorBrush(tin);
+            return true;
+        }
+
+        protected override bool TOutToTIn(SolidColorBrush tout, object parameter, CultureInfo culture, out Color tin)
+        {
+            tin = tout.Color;
+            return true;
+        }
+    }
+}
